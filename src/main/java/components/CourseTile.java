a6534ffba5f9f208ba98def8c93c4ab9ff0c68b0
NobodyThe1 <117182134+NobodyThe1@users.noreply.github.com>
2023-03-28 21:24:08 +0300

    Add files via upload

--- src/main/java/components/CourseTile.java
+++ src/main/java/components/CourseTile.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.support.FindBy;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.Temporal;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import data.MonthData;
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 public class CourseTile extends AbsComponent<CourseTile> {
 
@@ -25,24 +27,44 @@ public class CourseTile extends AbsComponent<CourseTile> {
     @FindBy(css = ".lessons__new-item-time")
     private List<WebElement> courseSpecializationStart;
 
-    @FindBy (css = ".lessons__new-item-start")
+    @FindBy(css = ".lessons__new-item-start")
     private List<WebElement> courseStart;
 
-public String getEarlierCourse() {
-    List <LocalDate> courseSpecializationStartDate = courseSpecializationStart.stream()
-            .map(WebElement::getText)
-            .filter(String -> String.length() > 10)
-            .map(String -> (String.replaceAll("\\s\\d.*", "")))
-            .map((String dateOfStart) -> {
-                String monthOfStart = dateOfStart.split("\\s+")[1];
-                dateOfStart = dateOfStart.replaceAll("[а-я]+", String.format("%d", MonthData.getDate(monthOfStart).getNumber()));
-                dateOfStart += " " + LocalDate.now().getYear();
-                        return LocalDate.parse(dateOfStart, DateTimeFormatter.ofPattern("d M yyyy"));
-                    }
-            )
-            .sorted(Comparator.naturalOrder())
-            .collect(Collectors.toList());
+    public void getEarlierCourse() {
+        List<LocalDateTime> allCoursesStarsDate = new ArrayList<>();
 
-    return courseSpecializationStartDate.get(0).toString();
-}
+        List<String> courseSpecializationStartString = courseSpecializationStart.stream()
+                .map(WebElement::getText)
+                .filter(String -> String.length() > 10)
+                .map(String -> (String.replaceAll("\\s\\d.*", "")))
+                .collect(Collectors.toList());
+
+        List<String> courseStartString = courseStart.stream()
+                .map(WebElement::getText)
+                .map(String -> (String.replaceAll("[С]\\s", "")))
+                .collect(Collectors.toList());
+
+        List<String> allCoursesStartString = new ArrayList<>();
+        Stream.of(courseSpecializationStartString, courseStartString)
+                .forEach(allCoursesStartString::addAll);
+
+       // List<> allCoursesStartDate =
+                allCoursesStartString.stream()
+                        .map((String dateOfEvent) ->
+                        {String monthOfEvent = dateOfEvent.split("\\s+")[1];
+                            dateOfEvent = dateOfEvent.replaceAll("[а-я]+", String.format("%d", MonthData.getDate(monthOfEvent).getNumber()));
+                            dateOfEvent += " " + LocalDate.now().getYear();
+
+                    if (dateOfEvent.equals("О дате старта будет объявлено позже")) {
+                        return null;
+                    } else if (dateOfEvent.equals("В сентябре")) {
+                        return LocalDate.of(2023, 9, 29);
+                    } else {
+                        return LocalDateTime.parse(dateOfEvent, DateTimeFormatter.ofPattern("d M yyyy"));
+                    }
+
+
+                })
+                .forEach(allCoursesStartString::sort);
+    }
 }
--- src/main/java/waiters/Waiter.java
+++ src/main/java/waiters/Waiter.java
@@ -1,11 +1,13 @@
+
 package waiters;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
+
 
 import java.time.Duration;
-
 public class Waiter {
 
     private WebDriver driver;
@@ -15,7 +17,7 @@ public class Waiter {
     }
 
     public boolean waitForCondition (ExpectedCondition condition) {
-        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
         try {
             webDriverWait.until(condition); {
                 return true;
